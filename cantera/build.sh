fbf962f6938c92516b26a641982fad2049f215da
Bryan W. Weber <bryan@bryanwweber.com>
2015-12-24 08:24:22 -0500

    [cantera] Fix typo in build.sh

--- cantera/build.sh
+++ cantera/build.sh
@@ -5,7 +5,7 @@ conda create -y -n cantera-builder python=2 numpy scons cython
 CANTER_MINOR_VER=${PKG_VERSION:2:1}
 PY_MAJ_VER=${PY_VER:0:1}
 
-if [ "${PY_MAR_VER}" == "2" ]; then
+if [ "${PY_MAJ_VER}" == "2" ]; then
     pip install 3to2
 fi
 
