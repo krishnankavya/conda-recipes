51194e1469a4dfefae1863448f8da8061e104c18
Bryan W. Weber <bryan@bryanwweber.com>
2015-12-23 23:57:00 -0500

    [cantera] Modify build command to use env vars
    Use the $PYTHON environment variable in scons build command. Also
    explicitly specify the python_cmd for Python 2 building, to make sure
    the right headers are found.

--- cantera/build.sh
+++ cantera/build.sh
@@ -13,9 +13,9 @@ if [ "${PKG_VERSION:2:1}" -gt "2" ]; then
 fi
 
 if [ "${PY_VER:0:1}" == "2" ]; then
-    scons build -j$((CPU_COUNT/2)) python3_package='n' python_package='full' matlab_toolbox='n' f90_interface='n'
+    scons build -j$((CPU_COUNT/2)) python3_package='n' python_cmd=$PYTHON python_package='full' matlab_toolbox='n' f90_interface='n'
 else
-    scons build -j$((CPU_COUNT/2)) python3_package='y' python3_cmd=${PREFIX}/bin/python python_package='none' matlab_toolbox='n' f90_interface='n'
+    scons build -j$((CPU_COUNT/2)) python3_package='y' python3_cmd=$PYTHON python_package='none' matlab_toolbox='n' f90_interface='n'
 fi
 
 conda env remove -y -n cantera-builder
