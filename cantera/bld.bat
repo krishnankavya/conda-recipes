b48259f968b5793cbd17d7b66fc7233038e04cfb
Bryan W. Weber <bryan@bryanwweber.com>
2015-12-24 07:51:36 -0500

    [cantera] Install 3to2 when building for Python 2

--- cantera/bld.bat
+++ cantera/bld.bat
@@ -3,12 +3,14 @@ conda env remove -y -n cantera-build
 conda create -y -n cantera-build python=2.7 cython numpy pywin32
 conda install -y --use-local -n cantera-build scons
 
-SET OLD_PATH=%PATH%
-SET PATH=%PREFIX:~0,-6%cantera-build\bin;%PREFIX:~0,-6%cantera-build\Scripts;%PATH%
-
 SET PY_MAJ_VER=%PY_VER:~0,1%
 SET CANTERA_MINOR_VER=%PKG_VERSION:~2,1%
 SET /A CPU_USE=%CPU_COUNT% / 2
+
+IF %PY_MAJ_VER% EQU 2 pip install 3to2
+
+SET OLD_PATH=%PATH%
+SET PATH=%PREFIX:~0,-6%cantera-build\bin;%PREFIX:~0,-6%cantera-build\Scripts;%PATH%
 
 CALL scons clean
 
@@ -33,6 +35,7 @@ GOTO BUILD_SUCCESS
 :BUILD_SUCCESS
 conda env remove -y -n cantera-build
 SET PATH=%OLD_PATH%
+IF %PY_MAJ_VER% EQU 2 pip uninstall -y 3to2
 
 cd interfaces/cython
 "%PYTHON%" setup%PY_MAJ_VER%.py build --build-lib=../../build/python%PY_MAJ_VER% install
--- cantera/build.sh
+++ cantera/build.sh
@@ -2,17 +2,23 @@
 
 conda env remove -y -n cantera-builder
 conda create -y -n cantera-builder python=2 numpy scons cython
+CANTER_MINOR_VER=${PKG_VERSION:2:1}
+PY_MAJ_VER=${PY_VER:0:1}
+
+if [ "${PY_MAR_VER}" == "2" ]; then
+    pip install 3to2
+fi
+
 OLD_PATH=$PATH
 export PATH=${PREFIX%_build}cantera-builder/bin:$PATH
 
 scons clean
-CANTER_VER=`grep -m 1 "cantera_version" SConstruct | sed "s/^.*cantera_version.*, '\(.*\)').*$/\1/"`
 
-if [ "${PKG_VERSION:2:1}" -gt "2" ]; then
+if [ "${CANTERA_MINOR_VER}" -gt "2" ]; then
     echo "system_sundials='n'" >> cantera.conf
 fi
 
-if [ "${PY_VER:0:1}" == "2" ]; then
+if [ "${PY_MAJ_VER}" == "2" ]; then
     scons build -j$((CPU_COUNT/2)) python3_package='n' python_cmd=$PYTHON python_package='full' matlab_toolbox='n' f90_interface='n'
 else
     scons build -j$((CPU_COUNT/2)) python3_package='y' python3_cmd=$PYTHON python_package='none' matlab_toolbox='n' f90_interface='n'
@@ -22,5 +28,9 @@ conda env remove -y -n cantera-builder
 
 export PATH=$OLD_PATH
 
+if [ "${PY_MAJ_VER}" == "2" ]; then
+    pip uninstall -y 3to2
+fi
+
 cd interfaces/cython
-$PYTHON setup${PY_VER:0:1}.py build --build-lib=../../build/python${PY_VER:0:1} install
+$PYTHON setup${PY_MAJ_VER}.py build --build-lib=../../build/python${PY_MAJ_VER} install
