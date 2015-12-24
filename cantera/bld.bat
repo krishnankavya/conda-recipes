1f021a4e060b3c208c9a5c8368c74835abc7e798
Bryan W. Weber <bryan@bryanwweber.com>
2015-12-24 14:13:01 -0500

    [cantera] Set up for building the stable version v2.2.0

--- cantera/bld.bat
+++ cantera/bld.bat
@@ -4,7 +4,6 @@ conda create -y -n cantera-build python=2.7 cython numpy pywin32
 conda install -y --use-local -n cantera-build scons
 
 SET PY_MAJ_VER=%PY_VER:~0,1%
-SET CANTERA_MINOR_VER=%PKG_VERSION:~2,1%
 SET /A CPU_USE=%CPU_COUNT% / 2
 
 IF %PY_MAJ_VER% EQU 2 pip install 3to2
@@ -16,26 +15,25 @@ CALL scons clean
 
 ECHO msvc_version='14.0' >> cantera.conf
 ECHO env_vars='all' >> cantera.conf
-
-IF %CANTERA_MINOR_VER% GTR 2 ECHO system_sundials='n' >> cantera.conf
+ECHO matlab_toolbox='n' >> cantera.conf
+ECHO f90_interface='n' >> cantera.conf
 
 IF %PY_MAJ_VER% EQU 2 GOTO PYTHON2
 IF %PY_MAJ_VER% EQU 3 GOTO PYTHON3
 
 :PYTHON2
 ECHO Building for Python 2
-CALL scons build -j%CPU_COUNT% python3_package=n python_cmd="%PYTHON%" python_package=full matlab_toolbox=n f90_interface=n
+CALL scons build -j%CPU_COUNT% python3_package=n python_cmd="%PYTHON%" python_package=full
 GOTO BUILD_SUCCESS
 
 :PYTHON3
 ECHO Building for Python 3
-CALL scons build -j%CPU_COUNT% python3_package=y python3_cmd="%PYTHON%" python_package=none matlab_toolbox=n f90_interface=n
+CALL scons build -j%CPU_COUNT% python3_package=y python3_cmd="%PYTHON%" python_package=none
 GOTO BUILD_SUCCESS
 
 :BUILD_SUCCESS
 conda env remove -y -n cantera-build
 SET PATH=%OLD_PATH%
-IF %PY_MAJ_VER% EQU 2 pip uninstall -y 3to2
 
 cd interfaces/cython
 "%PYTHON%" setup%PY_MAJ_VER%.py build --build-lib=../../build/python%PY_MAJ_VER% install
--- cantera/build.sh
+++ cantera/build.sh
@@ -2,7 +2,6 @@
 
 conda env remove -y -n cantera-builder
 conda create -y -n cantera-builder python=2 numpy scons cython
-CANTER_MINOR_VER=${PKG_VERSION:2:1}
 PY_MAJ_VER=${PY_VER:0:1}
 
 if [ "${PY_MAJ_VER}" == "2" ]; then
@@ -14,23 +13,19 @@ export PATH=${PREFIX%_build}cantera-builder/bin:$PATH
 
 scons clean
 
-if [ "${CANTERA_MINOR_VER}" -gt "2" ]; then
-    echo "system_sundials='n'" >> cantera.conf
-fi
+# We want neither the MATLAB interface nor the Fortran interface
+echo "matlab_toolbox='n'" >> cantera.conf
+echo "f90_interface='n'" >> cantera.conf
 
 if [ "${PY_MAJ_VER}" == "2" ]; then
-    scons build -j$((CPU_COUNT/2)) python3_package='n' python_cmd=$PYTHON python_package='full' matlab_toolbox='n' f90_interface='n'
+    scons build -j$((CPU_COUNT/2)) python3_package='n' python_cmd=$PYTHON python_package='full'
 else
-    scons build -j$((CPU_COUNT/2)) python3_package='y' python3_cmd=$PYTHON python_package='none' matlab_toolbox='n' f90_interface='n'
+    scons build -j$((CPU_COUNT/2)) python3_package='y' python3_cmd=$PYTHON python_package='none'
 fi
 
 conda env remove -y -n cantera-builder
 
 export PATH=$OLD_PATH
-
-if [ "${PY_MAJ_VER}" == "2" ]; then
-    pip uninstall -y 3to2
-fi
 
 cd interfaces/cython
 $PYTHON setup${PY_MAJ_VER}.py build --build-lib=../../build/python${PY_MAJ_VER} install
