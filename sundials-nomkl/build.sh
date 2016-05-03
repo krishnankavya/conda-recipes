477b5d0771d8f0f83af6dae99d1278046a02f17a
Bryan W. Weber <bryan@bryanwweber.com>
2016-05-03 16:16:07 -0400

    [sundials-nomkl] Add SUNDIALS recipe without MKL

---
+++ sundials-nomkl/build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+mkdir build
+cd build
+
+cmake $SRC_DIR -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DEXAMPLES_ENABLE=OFF -DEXAMPLES_INSTALL=OFF
+make -j$((CPU_COUNT/2))
+make install
