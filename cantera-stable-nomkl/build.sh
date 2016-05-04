551dea4032a9949e03c4d00adb21eda2128837fd
Bryan W. Weber <bryan@bryanwweber.com>
2016-05-04 17:09:21 -0400

    [cantera-stable-nomkl] Force cantera-builder numpy version to be the same as the conda-build version

--- cantera-stable-nomkl/build.sh
+++ cantera-stable-nomkl/build.sh
@@ -6,7 +6,7 @@ conda env remove -yq -n cantera-builder
 # Create a conda environment to build Cantera. It has to be Python 2, for
 # SCons compatibility. When SCons is available for Python 3, these machinations
 # can be removed
-conda create -yq -n cantera-builder python=2 numpy scons cython sundials nomkl
+conda create -yq -n cantera-builder python=2 numpy=$NPY_VER scons cython sundials nomkl
 
 # The major version of the Python that will be used for the installer, not the
 # version used for building
